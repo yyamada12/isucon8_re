4f53033f2fa9704956ee015242ef46c4ae42683c
yyamada12 <12yacropolisy@gmail.com>
2020-02-12 23:57:19 +0000

    resolve N + 1 in getEvent

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -236,24 +236,35 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
 		"C": &Sheets{},
 	}
 
+	reservationMap := make(map[int64]Reservation)
+	rows, err := db.Query("SELECT * FROM reservations WHERE event_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)", event.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var reservation Reservation
+		if err := rows.Scan(&reservation.ID, &reservation.EventID, &reservation.SheetID, &reservation.UserID, &reservation.ReservedAt, &reservation.CanceledAt); err != nil {
+			log.Fatal(err)
+		}
+		reservationMap[reservation.SheetID] = reservation
+	}
+
 	for _, rank := range []string{"S", "A", "B", "C"} {
 		for _, s := range sheetMap[rank] {
-            sheet := s
+			sheet := s
 			event.Sheets[sheet.Rank].Price = event.Price + sheet.Price
 			event.Total++
 			event.Sheets[sheet.Rank].Total++
 
-			var reservation Reservation
-			err := db.QueryRow("SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)", event.ID, sheet.ID).Scan(&reservation.ID, &reservation.EventID, &reservation.SheetID, &reservation.UserID, &reservation.ReservedAt, &reservation.CanceledAt)
-			if err == nil {
+			reservation, ok := reservationMap[sheet.ID]
+
+			if ok {
 				sheet.Mine = reservation.UserID == loginUserID
 				sheet.Reserved = true
 				sheet.ReservedAtUnix = reservation.ReservedAt.Unix()
-			} else if err == sql.ErrNoRows {
+			} else {
 				event.Remains++
 				event.Sheets[sheet.Rank].Remains++
-			} else {
-				return nil, err
 			}
 
 			event.Sheets[sheet.Rank].Detail = append(event.Sheets[sheet.Rank].Detail, &sheet)
